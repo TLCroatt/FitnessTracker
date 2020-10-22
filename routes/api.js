8eac2c64e832f1f28a8baa18f8ae34a763d36bbf
TLCroatt <trishacroatt@gmail.com>
2020-10-22 13:18:49 -0600

    updated api and html routes

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,52 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
+
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkouts);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(params.id, { $push: { exercises: body}},
+        //"runValidators" will ensure new exercises meet our schema requirements
+        { new: true, runValidators: true})
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.find()
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.get("/api/workouts/range", ({ query }, res) => {
+    Workout.find({ day: { $gte: query.start, $lte: query.end}})
+        .then(dbWorkouts => {
+            res.json(dbWorkouts);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+        .then(() => {
+            res.json(dbWorkouts);
+        }).catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
